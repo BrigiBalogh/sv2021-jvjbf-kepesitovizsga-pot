41858a0ec9d3f16d17190f2498164d970acf1212
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-12-01 10:51:16 +0100

    előkészítés kész

---
+++ src/test/java/training360/questions/Sv2021JvjbfKepesitovizsgaPotApplicationTests.java
@@ -0,0 +1,12 @@
+package training360.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Sv2021JvjbfKepesitovizsgaPotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
