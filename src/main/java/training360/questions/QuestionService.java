2b28686f6b469d185e464743119b071b4b8a0175
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-12-01 13:11:17 +0100

    Résztvevő létrehozás és lekérdezés kész

--- src/main/java/training360/questions/QuestionService.java
+++ src/main/java/training360/questions/QuestionService.java
@@ -3,6 +3,10 @@ package training360.questions;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import training360.questions.dto.CreateQuestionCommand;
+import training360.questions.dto.QuestionDto;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,4 +17,12 @@ public class QuestionService {
     private final ModelMapper mapper;
     private final QuestionRepository questionRepository;
 
+    public List<QuestionDto> getQuestions(Long id) {
+    }
+
+    public QuestionDto findQuestionById(Long id, Long questionId) {
+    }
+
+    public QuestionDto createQuestion(Long id, CreateQuestionCommand command) {
+    }
 }
