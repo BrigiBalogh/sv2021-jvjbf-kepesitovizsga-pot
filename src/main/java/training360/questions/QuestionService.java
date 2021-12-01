6c556996b39d29727a96b28cc3830ccc6b137fe1
BrigiBalogh <73614406+BrigiBalogh@users.noreply.github.com>
2021-12-01 13:14:52 +0100

    kész

--- src/main/java/training360/questions/QuestionService.java
+++ src/main/java/training360/questions/QuestionService.java
@@ -16,12 +16,16 @@ public class QuestionService {
 
     private final ModelMapper mapper;
     private final QuestionRepository questionRepository;
-
+    private final MemberService memberService;
     public List<QuestionDto> getQuestions(Long id) {
     }
 
     public QuestionDto findQuestionById(Long id, Long questionId) {
+        Member member =memberService.findById(id);
+        return mapper.map(lessonCompletion, LessonCompletionDto.class);
     }
+
+
 
     public QuestionDto createQuestion(Long id, CreateQuestionCommand command) {
     }
